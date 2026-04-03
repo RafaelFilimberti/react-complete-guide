a0bd8b4283c892a17222f03bc3e24536da2e3fe8
rfilimberti <rfilimberti@gmail.com>
2026-04-03 12:09:30 -0300

    refatoracao do codigo

--- 01-starting-project/src/components/CoreConcept.jsx
+++ 01-starting-project/src/components/CoreConcept.jsx
@@ -8,6 +8,7 @@ export default function CoreConcepts() {
             <ul>
                 {CORE_CONCEPTS.map((conceptItem) =>
                     <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+                    
                 {/* <CoreConcept
                       title={CORE_CONCEPTS[0].title}
                       description={CORE_CONCEPTS[0].description}
